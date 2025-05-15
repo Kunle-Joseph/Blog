06090cb23074331853302f69fdf989b859aeaa85
Kunle Joseph <olakunle.jos@gmail.com>
2025-05-15 13:47:03 +0100

    1.1.3

--- src/app/components/PostUpload.tsx
+++ src/app/components/PostUpload.tsx
@@ -48,27 +48,8 @@ export function PostUpload({
   };
 
   const compressImage = async (file: File): Promise<File> => {
-    if (file.type === "image/webp") {
+    if (file.type === "image/webp" || file.type === "image/gif") {
       return file; // No compression needed for webp
-    }
-
-    if (file.type == "image/gif") {
-      const options = {
-        maxSizeMB: 1, // Max file size in MB
-        maxWidthOrHeight: 1920, // Max width/height in pixels
-        useWebWorker: true, // Use web worker for better performance
-        initialQuality: 0.8, // Initial quality (0.8 = 80%)
-      };
-
-      try {
-        const compressedFile = await imageCompression(file, options);
-        return new File([compressedFile], file.name, {
-          type: compressedFile.type,
-        });
-      } catch (error) {
-        console.error("Error compressing image:", error);
-        throw error;
-      }
     }
 
     const options = {
